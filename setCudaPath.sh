4f4b7142350b309c744885e518ef8f373deef0a9
Christopher Loessl <cloessl@x-berg.de>
2010-07-06 10:59:16 +0200

    Now compiles on ibr machines

---
+++ setCudaPath.sh
@@ -0,0 +1,11 @@
+#export PATH=$PATH:/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda/bin:/media/HASSTICK/cuda/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda/lib64:/media/HASSTICK/cuda/lib64
+#alias make='make cuda-install=/media/HASSTICK/cuda'
+alias make='make cuda-install=/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda'
+
+
+
+export PATH=$PATH:/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/tu-bs.de/usr/usr53/y0053765/gicu/loessl/gicu:/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/tu-bs.de/usr/usr53/y0053765/cuda/cuda/lib64
+
